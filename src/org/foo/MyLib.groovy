efaa7144130987cd036e1238fddbba6bb12c9ac8
Aranel Doriath <aranelofdoriath@gmail.com>
2017-02-02 10:54:26 +0200

    third commit

--- src/org/foo/MyLib.groovy
+++ src/org/foo/MyLib.groovy
@@ -1,8 +1,11 @@
 package org.foo;
 
 class MyLib {
-	void build1() {
-		println "build 1"
+	void build_echo() {
+		echo 'build 1'
+	}
+	void build_input() {
+		input 'Waiting for your orders, massssster'
 	}
 	int summ(int a, int b) {
 		return a+b
